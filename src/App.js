b784063b3c0811abf7bb047831081f7af578d369
Leo Clarke <leojclarke@gmail.com>
2019-05-21 17:15:50 +0200

    Add second ScoreCard

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import ScoreCard from './ScoreCard';
 
 function App() {
   return (
-    <ScoreCard
-      title="new tickets"
-      value="20"
-      type="new"
-      dateFrom="01.01.2019"
-      dateTo="01.05.2019"
-    />
+    <section className="ScoreCards-Container">
+      <ScoreCard
+        title="new tickets"
+        value="20"
+        type="new"
+        dateFrom="01.01.2019"
+        dateTo="01.05.2019"
+      />
+
+      <ScoreCard
+        title="tickets in progress"
+        value="25"
+        type="wip"
+        dateFrom="01.01.2019"
+        dateTo="01.05.2019"
+      />
+    </section>
   );
 }
 
